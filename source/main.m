9446965431fb34ee5b8164b3ac0a54e03252a559
kyutei11 <152922303+kyutei11@users.noreply.github.com>
2023-12-05 19:52:30 +0900

    Update main.m

--- source/main.m
+++ source/main.m
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
-# E-mail : koichi_takasaki@mac.com
+# E-mail : koichi.takasaki@gmail.com
 
 clear
 
